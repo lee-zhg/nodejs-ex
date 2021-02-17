a7f5e2ff0cb61f31cfbe815716cd1148c952b5a3
Oliver Rodriguez <odrodrig@us.ibm.com>
2021-02-16 23:45:16 -0600

    Fixing odo install script

--- install_odo.sh
+++ install_odo.sh
@@ -5,7 +5,3 @@ echo "Installing odo"
 curl -L https://mirror.openshift.com/pub/openshift-v4/clients/odo/latest/odo-linux-amd64 -o ./odo
 
 chmod +x ./odo
-
-echo 'export PATH=/home/project/nodejs-ex:$PATH' > .bash_profile
-
-source .bash_profile
